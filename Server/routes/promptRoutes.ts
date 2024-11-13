da584e25f5be1b859b2b54a4b11f3140f1da2e58
macmini62 <victorkangacha@gmail.com>
2024-11-14 02:05:51 +0300

    changed the route for fetching chat prompts

--- Server/routes/promptRoutes.ts
+++ Server/routes/promptRoutes.ts
@@ -12,7 +12,7 @@ class PromptRouter{
 
   private routes(){
     PromptRouter.route.get("/prompt", (req, res) => this.botController.requestResponse(req, res));
-    PromptRouter.route.get("/prompt/:chat_id", (req, res) => this.botController.fetchPrompts(req, res))
+    PromptRouter.route.get("/prompts/:chat_id", (req, res) => this.botController.fetchPrompts(req, res))
   }
 }
 
